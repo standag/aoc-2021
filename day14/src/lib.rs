d49d9a5f0e167ea0e508ae6e61470b044c379828
Stanislav Geidl <standag@gmail.com>
2021-12-29 00:53:19 +0100

    day 14, second part

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -14,15 +14,28 @@ pub fn solve_part_1() {
     println!("Result: {}", polymer.calc_1());
 }
 
+fn calc(chain: &HashMap<String, u128>, first: &char) -> u128 {
+    let mut counter = HashMap::new();
+    chain.iter().for_each(|s| {
+        let c = s.0.chars().nth(1).unwrap();
+        let value = counter.entry(c).or_insert(0);
+        *value += s.1;
+    });
+    let value = counter.entry(*first).or_insert(0);
+    *value += 1;
+    counter.values().max().unwrap() - counter.values().min().unwrap()
+}
+
 pub fn solve_part_2() {
     let mut test_polymer = Polymer::from_file("day14/input_test.txt");
-    test_polymer.polymerize_loop_opt(4);
-    test_polymer.print();
-    println!("Test result: {}", test_polymer.calc_1());
+    let test_chain = test_polymer.polymerize_loop_opt(40);
+    let test_result = calc(&test_chain, &test_polymer.chain.first().unwrap());
+    println!("Test result: {:?}", test_result);
 
     let mut polymer = Polymer::from_file("day14/input.txt");
-    polymer.polymerize_loop_opt(40);
-    println!("Result: {}", polymer.calc_1());
+    let final_chain = polymer.polymerize_loop_opt(40);
+    let result = calc(&final_chain, &polymer.chain[0]);
+    println!("Result: {:?}", result);
 }
 
 #[derive(Debug)]
@@ -46,7 +59,7 @@ impl Polymer {
         });
         Polymer {
             chain: raws[0].chars().collect::<Vec<char>>().to_vec(),
-            rules: rules,
+            rules,
         }
     }
 
@@ -69,39 +82,31 @@ impl Polymer {
         })
     }
 
-    fn polymerize_loop_opt(&mut self, number_of_cycles: u8) {
-        let mut new_chain = self.chain.to_vec();
-        let mut p = 0;
-        loop {
-            for i in 0..number_of_cycles {
-                let key = new_chain[p].to_string() + &new_chain[p + 1].to_string();
-                let value = *self.rules.get(&key).unwrap();
-                new_chain.insert(p + 1, value);
-                for _ in 0..i {
-                    let ui  = i as usize;
-                    let key = new_chain[p + ui].to_string() + &new_chain[p + ui + 1].to_string();
-                    let value = *self.rules.get(&key).unwrap();
-                    new_chain.insert(p + ui + 1, value);
-                }
+    fn polymerize_loop_opt(&mut self, number_of_cycles: u8) -> HashMap<String, u128> {
+        let mut chain = HashMap::new();
+        let mut new_chain = HashMap::new();
+        (0..(self.chain.len() - 1)).for_each(|i| {
+            let key = self.chain[i].to_string() + &self.chain[i + 1].to_string();
+            if let Some(value) = chain.get_mut(&key) {
+                *value += 1;
+            } else {
+                chain.insert(key, 1);
             }
-            p = new_chain.len() - (self.chain.len() - p);
-            if p >= new_chain.len() {
-                break;
-            };
-            println!("Pointer: {} {}", p, new_chain.len());
-        } /*
-          for i in 0..self.chain.len() - 1 {
-              new_chain.push(self.chain[i]);
-              let second = self.chain[i + 1];
-              for j in 0..number_of_cycles {
-                  let key = new_chain[j as usize].to_string() + &new_chain[j as usize].to_string();
-                  let value = *self.rules.get(&key).unwrap();
-                  new_chain.push(value);
-              }
-              println!("Polymer part: {} {:?}", i, new_chain);
-          }
-          new_chain.push(*self.chain.last().unwrap());*/
-        self.chain = new_chain;
+        });
+        for _ in 0..number_of_cycles {
+            chain.iter().for_each(|x| {
+                let c = self.rules.get(x.0).unwrap();
+                let key1 = x.0.chars().nth(0).unwrap().to_string() + &c.to_string();
+                let key2 = c.to_string() + &x.0.chars().nth(1).unwrap().to_string();
+                let value1 = new_chain.entry(key1).or_insert(0);
+                *value1 += x.1;
+                let value2 = new_chain.entry(key2).or_insert(0);
+                *value2 += x.1;
+            });
+            chain = new_chain.clone();
+            new_chain = HashMap::new();
+        }
+        chain
     }
 
     fn print(&self) {
